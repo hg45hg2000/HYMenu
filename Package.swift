7259ff20fee0c16ee23b008db31054acd26d22b3
henry <henry@yeontech.com>
2024-07-16 10:15:15 +0800

    cocoapods

--- Package.swift
+++ Package.swift
@@ -11,11 +11,17 @@ let package = Package(
             name: "HYMenu",
             targets: ["HYMenu"]),
     ],
+    dependencies: [
+            // Dependencies declare other packages that this package depends on.
+             .package(url: "https://github.com/hg45hg2000/HYMenu.git", from: "1.1.1"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HYMenu"),
+            name: "HYMenu",
+            dependencies: [],
+                        path: "HYMenu/Classes"),
         .testTarget(
             name: "HYMenuTests",
             dependencies: ["HYMenu"]),
